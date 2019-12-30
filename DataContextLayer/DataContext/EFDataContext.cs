874caf0a133be6baff60ad083b8b91bd41ef5e9a
azampunya25 <zamzamcms@gmail.com>
2019-12-30 16:20:44 +0700

    intial commit

---
+++ DataContextLayer/DataContext/EFDataContext.cs
@@ -0,0 +1,20 @@
+﻿using DataContextLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataContextLayer.DataContext
+{
+    public class EFDataContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Designation> Designations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=FMF-HO-PROG4;Initial Catalog=SIG;Integrated Security=False;Persist Security Info=False;User ID=sa;Password=finansia");
+        }
+    }
+}
---
+++ SIGApp/Controllers/DesignationsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataContextLayer.DataContext;
+using DataContextLayer.Models;
+
+namespace SIGApp.Controllers
+{
+    public class DesignationsController : Controller
+    {
+        private readonly EFDataContext _context;
+
+        public DesignationsController(EFDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Designations
+        public async Task<IActionResult> Index()
+        {
+            var eFDataContext = _context.Designations.Include(d => d.DepartmentInfo);
+            return View(await eFDataContext.ToListAsync());
+        }
+
+        // GET: Designations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var designation = await _context.Designations
+                .Include(d => d.DepartmentInfo)
+                .FirstOrDefaultAsync(m => m.DesignationId == id);
+            if (designation == null)
+            {
+                return NotFound();
+            }
+
+            return View(designation);
+        }
+
+        // GET: Designations/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentCode");
+            return View();
+        }
+
+        // POST: Designations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DesignationId,DesignationCode,DesignationName,DepartmentId")] Designation designation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(designation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentCode", designation.DepartmentId);
+            return View(designation);
+        }
+
+        // GET: Designations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var designation = await _context.Designations.FindAsync(id);
+            if (designation == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentCode", designation.DepartmentId);
+            return View(designation);
+        }
+
+        // POST: Designations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DesignationId,DesignationCode,DesignationName,DepartmentId")] Designation designation)
+        {
+            if (id != designation.DesignationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(designation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DesignationExists(designation.DesignationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "DepartmentCode", designation.DepartmentId);
+            return View(designation);
+        }
+
+        // GET: Designations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var designation = await _context.Designations
+                .Include(d => d.DepartmentInfo)
+                .FirstOrDefaultAsync(m => m.DesignationId == id);
+            if (designation == null)
+            {
+                return NotFound();
+            }
+
+            return View(designation);
+        }
+
+        // POST: Designations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var designation = await _context.Designations.FindAsync(id);
+            _context.Designations.Remove(designation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DesignationExists(int id)
+        {
+            return _context.Designations.Any(e => e.DesignationId == id);
+        }
+    }
+}
